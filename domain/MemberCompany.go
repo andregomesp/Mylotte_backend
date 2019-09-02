9ab2e9985a235de6c6603e6f646a53cba7fbf9b8
andregomesp <andregomespin@gmail.com>
2019-09-02 08:06:57 -0300

    go init

---
+++ domain/MemberCompany.go
@@ -0,0 +1,7 @@
+package domain
+
+type MemberCompany struct {
+	id uint64
+	Member Member
+
+}
---
+++ domain/UserData.go
@@ -0,0 +1,13 @@
+package domain
+
+type  user_data struct {
+	id uint64
+	username string
+	password string
+	profile string
+	name string
+	email string
+	birthDate string
+	isEnabled string
+	isVerified string
+}
