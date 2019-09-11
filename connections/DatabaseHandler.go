5ae46b85deada672e776ead2600a42b0f5569acd
andregomesp <andregomespin@gmail.com>
2019-09-11 02:17:36 -0300

    bngfgnjh

--- connections/DatabaseHandler.go
+++ connections/DatabaseHandler.go
@@ -3,12 +3,35 @@ package connections
 import (
 	"context"
 	"database/sql"
+	"log"
 )
 
-func prepareContext(db *sql.DB) {
 
+func PrepareContext(db *sql.DB) {
+	ctx, cancel :=
 }
 
-func createTransaction(db *sql.DB, context context.Context) *sql.Tx {
-	tx, err := db.BeginTx(context)
+func CreateTransaction(db *sql.DB) *sql.Tx {
+
+	tx, err := db.BeginTx(ctx, &sql.TxOptions{Isolation: sql.LevelSerializable})
+	if err != nil {panic(err)}
+	return tx
+}
+
+func CommitTransaction(tx *sql.Tx) bool {
+	err := tx.Commit()
+	commit := true
+	if err != nil {
+		commit = false
+		log.Print(err)
+		RollbackTransaction(tx)
+	}
+	return commit
+}
+
+func RollbackTransaction(tx *sql.Tx) {
+	err := tx.Rollback()
+	if err != nil {
+		log.Println(err)
+	}
 }
